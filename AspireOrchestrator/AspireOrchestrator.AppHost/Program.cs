fc6923867921078061f54f3027927723374abdeb
Tommaso Stocchi <tstocchi@microsoft.com>
2024-05-28 14:00:07 +0200

    uses local container for local run

--- AspireOrchestrator/AspireOrchestrator.AppHost/Program.cs
+++ AspireOrchestrator/AspireOrchestrator.AppHost/Program.cs
@@ -2,28 +2,38 @@ using Aspire.Hosting.Dapr;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var insights = builder.AddAzureApplicationInsights("appin-distributed-calculator");
+// To use this, set a secret in the project for the connection string named appin-distributed-calculator
+var insights = builder.ExecutionContext.IsPublishMode
+    ? builder.AddAzureApplicationInsights("appin-distributed-calculator")
+    : builder.AddConnectionString("appin-distributed-calculator", "APPLICATIONINSIGHTS_CONNECTION_STRING");
 
 // Configure Adder in Go
-var add = builder.AddContainer("addapp", "acr3stty56cqa3yc.azurecr.io/addapp")
+var add = builder.ExecutionContext.IsPublishMode
+    ? builder.AddContainer("addapp", "acrt6xtihl2b3uxe.azurecr.io/addapp")
+    : builder.AddContainer("addapp", "addapp")
     .WithHttpEndpoint(targetPort: 6000, env: "APP_PORT", name: "http")
     .PublishAsContainer();
 var addEnpoint = add.GetEndpoint("http");
 
 // Configure Multiplier in Python
-var multiply = builder.AddContainer("multiplyapp", "acr3stty56cqa3yc.azurecr.io/multiplyapp")
+var multiply = builder.ExecutionContext.IsPublishMode
+    ? builder.AddContainer("multiplyapp", "acrt6xtihl2b3uxe.azurecr.io/multiplyapp")
+    : builder.AddContainer("multiplyapp", "multiplyapp")
     .WithHttpEndpoint(targetPort: 5001, env: "APP_PORT", name: "http")
     .PublishAsContainer();
 var multiplyEnpoint = multiply.GetEndpoint("http");
 
 // Configure Divider in NodeJS
-var divide = builder.AddContainer("divideapp", "acr3stty56cqa3yc.azurecr.io/divideapp")
+var divide = builder.ExecutionContext.IsPublishMode
+    ? builder.AddContainer("divideapp", "acrt6xtihl2b3uxe.azurecr.io/divideapp")
+    : builder.AddContainer("divideapp", "divideapp")
     .WithHttpEndpoint(targetPort: 4000, env: "APP_PORT", name: "http")
     .PublishAsContainer();
 var divideEnpoint = divide.GetEndpoint("http");
 
 // Configure Subtractor in .NET
-var subtract = builder.AddProject<Projects.dotnet_subtractor>("subtractapp");
+var subtract = builder.AddProject<Projects.dotnet_subtractor>("subtractapp")
+    .WithReference(insights);
 
 // Configure Dapr State Store
 var stateStore = builder.AddDaprStateStore("statestore");
